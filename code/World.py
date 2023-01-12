f6de5248e2ec42250a27af06d90270812d8afed9
Henri <henrib2001@gmail.com>
2023-01-12 17:48:53 +0100

    wip(main):setup Level

---
+++ code/World.py
@@ -0,0 +1,13 @@
+import pygame
+
+class World:
+    def __init__(self):
+        #get the display surface
+        self.display_surface = pygame.display.get_surface()
+        #sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
+        
+    def run(self):
+        #update and draw the game
+        pass
--- code/main.py
+++ code/main.py
@@ -1,23 +1,25 @@
 import pygame, sys
 from settings import *
-
+from debug import debug
+from World import World
 class Game:
 	def __init__(self):
 		  
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
+		pygame.display.set_caption('Jeu')
 		self.clock = pygame.time.Clock()
-	
+		self.world=World()
+  
 	def run(self):
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-
 			self.screen.fill('black')
+			self.world.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
 
