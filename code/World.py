514b99f2b96d932bb5e3e9892425b8ee285b8608
Henri <henrib2001@gmail.com>
2023-01-12 18:39:45 +0100

    wip(Jeu): world and moves

--- code/World.py
+++ code/World.py
@@ -1,5 +1,8 @@
 import pygame
-
+from settings import *
+from tile import Tile 
+from player import Player
+from debug import debug
 class World:
     def __init__(self):
         #get the display surface
@@ -7,7 +10,24 @@ class World:
         #sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
-        
+    
+        # sprite setup
+        self.create_map()
+    
+    def create_map(self):
+        for row_index,row in enumerate(WORLD_MAP):
+            #print(row_index,row)
+            for col_index,col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x,y),[self.visible_sprites,self.obstacles_sprites])
+                if col == 'p':
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacles_sprites)
+                    
     def run(self):
         #update and draw the game
         pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
